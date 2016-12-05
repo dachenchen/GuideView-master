dcfe1839367018abfeaf988249abd96e806602c4
chen <15060383743@163.com>
2016-12-05 16:32:47 +0800

    Initial commit

---
+++ demo/src/main/java/cn/lemon/guideview/GuideViewFragment.java
@@ -0,0 +1,92 @@
+package cn.lemon.guideview;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by linlongxin on 2016/7/25.
+ */
+
+public class GuideViewFragment extends Fragment {
+
+    private GuideView mGVOne, mGVTwo, mGVThree;
+    private View fragment;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        fragment = inflater.inflate(R.layout.fragment_guideview, container, false);
+        return fragment;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fragment.post(new Runnable() {
+            @Override
+            public void run() {
+                showGuideViews();
+            }
+        });
+    }
+
+    public void showGuideViews() {
+        TextView mHintViewOne = new TextView(getActivity());
+        mHintViewOne.setText("列夫·托尔斯泰曾在《安娜·卡列尼娜》里说过：幸福的家庭都是相似的");
+        mHintViewOne.setTextSize(20f);
+        mHintViewOne.setTextColor(Color.WHITE);
+
+        mGVOne = new GuideView.Builder(getActivity())
+                .setTargetView(R.id.text_one)
+                .setHintView(mHintViewOne)
+                // 设置字体位置
+                .setHintViewDirection(Direction.RIGHT_BOTTOM)
+                .setTransparentOvalPadding(20)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mGVOne.hide();
+                        mGVTwo.show();
+                    }
+                })
+                .create();
+        mGVOne.show();
+
+        mGVTwo = new GuideView.Builder(getActivity())
+                .setTargetView(R.id.text_two)
+                .setHintView(mHintViewOne)
+                .setHintViewDirection(Direction.BOTTOM_ALIGN_LEFT)
+                .setTransparentOvalPaddingLeft(20)
+                .setTransparentOvalPaddingRight(20)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mGVTwo.hide();
+                        mGVThree.show();
+                    }
+                })
+                .create();
+
+        mGVThree = new GuideView.Builder(getActivity())
+                .setTargetView(R.id.text_three)
+                .setHintView(mHintViewOne)
+                .setHintViewDirection(Direction.LEFT_BOTTOM)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mGVThree.hide();
+                    }
+                })
+                .create();
+    }
+}
---
+++ demo/src/main/java/cn/lemon/guideview/MainActivity.java
@@ -0,0 +1,18 @@
+package cn.lemon.guideview;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, new GuideViewFragment());
+        transaction.commit();
+    }
+}
